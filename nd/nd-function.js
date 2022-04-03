f4b1ce0714b46a9bf3dbb8188865a9b0070a63c1
Kristina Rudyte <rkristina32@yahoo.com>
2022-04-03 23:37:48 +0300

    initial commit

---
+++ nd/nd-function.js
@@ -0,0 +1,15 @@
+/*Parašyti funkciją, kuri priima du skaičius. Grąžina tų skaičių sumą.*/
+
+function suma(a,b) {
+    return a+b;
+}
+let s1 = suma(2, 3)
+console.log(`suma =`, s1);
+
+console.log('-----------------------------------');
+/*Parašyti funkciją, kuri priima du kintamuosius.Palygina tuos du skaičius ir grąžina rezultatą ar pirmas skaičius yra didesnis, ar antras, ar jie yra lygūs.*/
+function palyginimas (a, b) {
+   return a>b || a<b ||
+}
+let them = palyginimas(3, 4)
+console.log(them);
