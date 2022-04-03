504abd6f5461ab4bcce5a1ea84809c4e59062df4
Kristina Rudyte <rkristina32@yahoo.com>
2022-04-03 22:14:19 +0300

    nd 31 half

---
+++ nd.js
@@ -0,0 +1,35 @@
+//Naudojant ciklą atspausdinti penkias eilutes ‘Labas’.
+for (i=0; i<5; i++) {
+    console.log('Labas');
+}
+console.log('--------------------------');
+/*Naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 1 2 3 4 (vienas skaičius vienoje eilutėje).*/
+
+for (i=0; i<5; i++) {
+    console.log(i);
+}
+console.log('----------------------------');
+/*Naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 10 20 30 40 (vienas skaičius vienoje eilutėje).*/
+for (i=0; i<5; i++) {
+    console.log(i*10);
+}
+console.log('----------------------------------');
+/*Naudojant ciklą penkiose eilutėse atspausdinti skaičius 49 50 51 52 53 (vienas skaičius vienoje eilutėje).*/
+
+
+for (i=49; i<54; i++) {
+const diff = 0;
+    console.log(i);
+}
+console.log('-----------------------------');
+/*Naudojant ciklą penkiose eilutėse atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje).*/
+
+for (i=0; i<5; i+=1) {
+    console.log(i*2.5);
+}
+console.log('---------------------------------');
+/*Naudojant ciklą, atspausdinti skaičius nuo 1 iki 20. Nespausdinti skaičių, kurie dalinasi iš 3.*/
+for (i=1; i<21; i++) {
+    console.log(i>i/3);
+}
+
