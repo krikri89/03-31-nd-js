426aeb3775e438ef754f6673b4797220ddc14006
Kristina Rudyte <rkristina32@yahoo.com>
2022-04-04 18:03:47 +0300

    for fixed

--- nd/nd-for.js
+++ nd/nd-for.js
@@ -10,21 +10,22 @@ for (i = 0; i < 5; i++) {
 }
 console.log("3----------------------------");
 /*Naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 10 20 30 40 (vienas skaičius vienoje eilutėje).*/
-for (i = 0; i < 5; i++) {
-  console.log(i * 10);
+for (i = 0; i < 50; i+=10) {
+  console.log(i);
 }
 console.log("4----------------------------------");
 /*Naudojant ciklą penkiose eilutėse atspausdinti skaičius 49 50 51 52 53 (vienas skaičius vienoje eilutėje).*/
 
 for (i = 49; i < 54; i++) {
-  const diff = 0;
   console.log(i);
 }
 console.log("5-----------------------------");
 /*Naudojant ciklą penkiose eilutėse atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje).*/
-
-for (i = 0; i < 5; i += 1) {
-  console.log(i * 2.5);
+function rand(min, max){
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+for (i = 0; i < 5; i++) {
+  console.log(rand(0, 10));
 }
 console.log("6---------------------------------");
 /*Naudojant ciklą, atspausdinti skaičius nuo 1 iki 20. Nespausdinti skaičių, kurie dalinasi iš 3.*/
@@ -34,10 +35,11 @@ for (i = 1; i < 20; i ++) {
 }
 console.log("7----------------------------------");
 /*Parašykite programą, kuri atspausdintų įvesto skaičiaus daugybos lentelę nuo 1 iki 10.*/
+let b = 1;
 for (i = 0; i < 11; i+=1) {
-  let suma = 0;
-  let daugyba = i*i;
-  console.log(i, `x`, i, `= `, daugyba);
+  
+  let daugyba = i*b;
+  console.log(i, `x`, b, `= `, daugyba);
 }
 console.log("8------------------------------------");
 /*Vienas colis yra 2,54 cm. Sudarykite programą kuri pateiktų ekranelentelę nuo 1 iki n colius centimetrais.*/
@@ -48,15 +50,17 @@ for (i = 1; i < 10; i++) {
 
 console.log("9-------------------------------------");
 /*Bankas moka 2% metinių palūkanų. Indėlininkas pasidėjo į banką 100e. Sudarykite kasmetinę banko atskaitą klientui iki n metų. Ataskaitoje pateikite: metus, palūkanas, indėlį.*/
-
-for (i = 100; i < 110; i+=2) {
-const palukanos = i*0.02;
-console.log(`n metai + palukanu €`, palukanos, `=`, i, `€`);
+let n = 5;
+let suma =100;
+for (i = 1; i <= 5; i++) {
+const palukanos = suma*0.02;
+suma += palukanos;
+console.log(i, `metu + palukanu €`, palukanos.toFixed(2), `=`, suma.toFixed(2), `€`);
 }
 
 console.log('10------------------------------------');
 /*Keliamieji metai turi 366 dienas, paprastieji–365. Visi metai, išskyrus šimtmečius, yra keliamieji, jie dalūs iš 4. Šimtmečių metai yra keliamieji, jeigu dalūs 400. Pvz.: 1600 metai yra keliamieji, nes dalūs iš 400, o 1700 –paprastieji. Raskite visus mūsų eros keliamuosius metus.*/
 for (i = 1000; i < 2022; i++) {
-  if(i%4 === 0 && i%400 === 0)
+  if(i%4 === 0 && i%100 !== 0 || i%400 === 0)
   console.log(`Kelemieji metai`, i);
 }
--- nd/nd-function.js
+++ nd/nd-function.js
@@ -1,15 +1,52 @@
 /*Parašyti funkciją, kuri priima du skaičius. Grąžina tų skaičių sumą.*/
 
-function suma(a,b) {
-    return a+b;
+function suma(a, b) {
+  return a + b;
 }
-let s1 = suma(2, 3)
+let s1 = suma(2, 3);
 console.log(`suma =`, s1);
+let s11 = suma(-22, 3);
+console.log(`suma =`, s11);
+let s111 = suma(2, -33);
+console.log(`suma =`, s111);
 
-console.log('-----------------------------------');
 /*Parašyti funkciją, kuri priima du kintamuosius.Palygina tuos du skaičius ir grąžina rezultatą ar pirmas skaičius yra didesnis, ar antras, ar jie yra lygūs.*/
-function palyginimas (a, b) {
-   return a>b 
+console.log("--2---------------------------------");
+
+function palyginimas(a, b) {
+ 
+  if (a > b) {
+    console.log(a, `yra daugiau uz`, b);
+  } else if (a < b) {
+    console.log(a, `yra maziau uz`, b);
+  } else console.log(`skaiciai yra lygus`);
 }
-let them = palyginimas(3, 4)
-console.log(them);
+palyginimas(3, 4);
+palyginimas(5, 4);
+palyginimas(3, 3);
+
+
+/*Parašyti funkciją, kuri priimtų vieną skaičių. Turi būti grąžinamas rezultatas ar tie metai yra keliemieji.*/
+
+console.log("---3-------------------");
+
+function metai(a) {
+  if (a % 4 === 0) {
+    console.log(a, `metai yra keliamieji`);
+  } else console.log(a, "metai yra nekeliamieji");
+}
+metai(2000);
+
+metai(2022);
+
+
+/*Parašyti funkciją, kuri priimtų vieną skaičių. Grąžinti skaičiaus kvadratą.*/
+console.log('----4---------------');
+
+
+const kvadratas = a => a*a;
+
+const r2 = kvadratas(7);
+console.log(r2);
+
+/**/
